d8e969e4808727097d26ed5ba074015fb87a053a
adealbas <addielu@gmail.com>
2020-04-15 21:24:07 -0500

    Update Addiel_DBShiny.R

--- Addiel_DBShiny.R
+++ Addiel_DBShiny.R
@@ -1,1 +1,34 @@
+library(shiny)
+library(DBI)
+library(pool)
+library(dplyr)
+library(dbplyr)
+pool <- dbPool(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest"
+)
+
+ui <- fluidPage(
+  textInput("ID", "Enter your ID:", "5"),
+  tableOutput("tbl"),
+  numericInput("nrows", "How many cities to show?", 10),
+  plotOutput("popPlot")
+)
+
+server <- function(input, output, session) {
+  output$tbl <- renderTable({
+    pool %>% tbl("City") %>%
+      filter(ID == input$ID)
+  })
+  output$popPlot <- renderPlot({
+    df <- pool %>% tbl("City") %>%
+      head(as.integer(input$nrows)[1]) %>% collect()
+    pop <- df$Population
+    names(pop) <- df$Name
+    barplot(pop)
+  })
+}
 
